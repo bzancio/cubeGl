29f03e252a8cb4acac713a340e7eeb3f274f3510
bzancio <bzancio@gmail.com>
2025-10-15 13:20:02 +0200

    cubo rotando

--- src/main/java/com/cubeGl/Main.java
+++ src/main/java/com/cubeGl/Main.java
@@ -1,7 +1,12 @@
 package com.cubeGl;
 
+import org.joml.Matrix4f;
+import org.lwjgl.BufferUtils;
 import org.lwjgl.glfw.GLFW;
 import org.lwjgl.opengl.GL;
+
+import java.nio.FloatBuffer;
+
 import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.opengl.GL30.*;
 
@@ -31,19 +36,32 @@ public class Main {
         GLFW.glfwShowWindow(window);
 
         GL.createCapabilities();
+        glEnable(GL_DEPTH_TEST);
         glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 
-        setupSquare();
+        setupCube();
         setupShaders();
     }
 
     private void loop() {
+        FloatBuffer fb = BufferUtils.createFloatBuffer(16);
+
         while (!GLFW.glfwWindowShouldClose(window)) {
             glClear((GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT));
 
             glUseProgram(shaderProgram);
+
+            float time = (float)GLFW.glfwGetTime();
+            Matrix4f transform  = new Matrix4f()
+                    .rotateY(time * 0.5f)
+                    .rotateX(time * 0.5f);
+
+            int transforMLoc = glGetUniformLocation(shaderProgram, "transform");
+            transform.get(fb);
+            glUniformMatrix4fv(transforMLoc, false, fb);
+
             glBindVertexArray(vaoId);
-            glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+            glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
 
             GLFW.glfwSwapBuffers(window);
             GLFW.glfwPollEvents();
@@ -54,17 +72,26 @@ public class Main {
         new Main().run();
     }
 
-    private void setupSquare() {
+    private void setupCube() {
         float[] vertices = {
-                -0.5f,  0.5f,      1.0f, 0.0f, 0.0f,   // 0 - A (rojo)
-                0.5f,  0.5f,      0.0f, 1.0f, 0.0f,   // 1 - B (verde)
-                -0.5f, -0.5f,      0.0f, 0.0f, 1.0f,   // 2 - C (azul)
-                0.5f, -0.5f,      1.0f, 1.0f, 0.0f    // 3 - D (amarillo)
+                // posiciones        // colores (RGB)
+                -0.5f, -0.5f, -0.5f,   1.0f, 0.0f, 0.0f, // rojo
+                0.5f, -0.5f, -0.5f,   0.0f, 1.0f, 0.0f, // verde
+                0.5f,  0.5f, -0.5f,   0.0f, 0.0f, 1.0f, // azul
+                -0.5f,  0.5f, -0.5f,   1.0f, 1.0f, 0.0f, // amarillo
+                -0.5f, -0.5f,  0.5f,   1.0f, 0.0f, 1.0f, // magenta
+                0.5f, -0.5f,  0.5f,   0.0f, 1.0f, 1.0f, // cian
+                0.5f,  0.5f,  0.5f,   1.0f, 1.0f, 1.0f, // blanco
+                -0.5f,  0.5f,  0.5f,   0.0f, 0.0f, 0.0f  // negro
         };
 
         int[] indices = {
-                0, 1, 2,   // Primer triángulo (A-B-C)
-                1, 3, 2    // Segundo triángulo (B-D-C)
+                0, 1, 2, 2, 3, 0, // cara trasera
+                4, 5, 6, 6, 7, 4, // cara delantera
+                0, 4, 7, 7, 3, 0, // izquierda
+                1, 5, 6, 6, 2, 1, // derecha
+                3, 2, 6, 6, 7, 3, // arriba
+                0, 1, 5, 5, 4, 0  // abajo
         };
 
 
@@ -75,29 +102,31 @@ public class Main {
         glBindBuffer(GL_ARRAY_BUFFER, vboId);
         glBufferData(GL_ARRAY_BUFFER, vertices, GL_STATIC_DRAW);
 
-        int stride = (2 + 3) * Float.BYTES;
-
         int eboId = glGenBuffers();
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, eboId);
         glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices, GL_STATIC_DRAW);
 
-        glVertexAttribPointer(0, 2, GL_FLOAT, false, stride, 0L);
+        int stride = (3 + 3) * Float.BYTES;
+
+        glVertexAttribPointer(0, 3, GL_FLOAT, false, stride, 0);
         glEnableVertexAttribArray(0);
 
-        glVertexAttribPointer(1, 3, GL_FLOAT, false, stride, 2L * Float.BYTES);
+        glVertexAttribPointer(1, 3, GL_FLOAT, false, stride, 3 * Float.BYTES);
         glEnableVertexAttribArray(1);
     }
 
     private void setupShaders() {
         String vertexShaderSource = """
                 #version 330 core
-                 layout(location = 0) in vec2 position;
+                 layout(location = 0) in vec3 position;
                  layout(location = 1) in vec3 color;
                 
                  out vec3 vertexColor; // saldrá hacia el fragment shader
                 
+                 uniform mat4 transform;
+                
                  void main() {
-                     gl_Position = vec4(position, 0.0, 1.0);
+                     gl_Position = transform * vec4(position, 1.0);
                      vertexColor = color;
                  }""";
 
---
+++ src/main/java/com/cubeGl/Mesh.java
@@ -0,0 +1,4 @@
+package com.cubeGl;
+
+public class Mesh {
+}
